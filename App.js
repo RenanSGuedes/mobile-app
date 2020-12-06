9b28fc4419c9426915047f332b3915c3353bc36b
RenanSGuedes <renanguedes.br@gmail.com>
2020-12-06 16:08:40 -0300

    :atom:

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import InitialOptions from './src/pages/InitialOptions' //
-import Home from './src/pages/Home' //
+import InitialOptions from './src/pages/InitialOptions' 
+import Home from './src/pages/Home' 
 
-import A2_Temperature from './src/pages/A2_Temperature' //
-import B2_Temperature from './src/pages/B2_Temperature' //
-import NFT_Temperature from './src/pages/NFT_Temperature' //
+import A2_Temperature from './src/pages/A2_Temperature' 
+import B2_Temperature from './src/pages/B2_Temperature' 
+import NFT_Temperature from './src/pages/NFT_Temperature' 
 
-import A2_Flow from './src/pages/A2_Flow' //
-import B2_Flow from './src/pages/B2_Flow' //
+import A2_Flow from './src/pages/A2_Flow' 
+import B2_Flow from './src/pages/B2_Flow' 
 
-import A2_Humidity from './src/pages/A2_Humidity' //
-import B2_Humidity from './src/pages/B2_Humidity' //
+import A2_Humidity from './src/pages/A2_Humidity' 
+import B2_Humidity from './src/pages/B2_Humidity' 
 
 import A2_Level from './src/pages/A2_Level'
 import B2_Level from './src/pages/B2_Level'
 import NFT_Level from './src/pages/NFT_Level' 
 
-import ListPlot from './src/pages/ListPlot' //
-import A2_ListPlot from './src/pages/A2_ListPlot' //
-import B2_ListPlot from './src/pages/B2_ListPlot' //
-import NFT_ListPlot from './src/pages/NFT_ListPlot' //
+import ListPlot from './src/pages/ListPlot' 
+import A2_ListPlot from './src/pages/A2_ListPlot' 
+import B2_ListPlot from './src/pages/B2_ListPlot' 
+import NFT_ListPlot from './src/pages/NFT_ListPlot' 
 
 import A2_FlowPlot from './src/pages/A2_FlowPlot'
 import B2_FlowPlot from './src/pages/B2_FlowPlot'
